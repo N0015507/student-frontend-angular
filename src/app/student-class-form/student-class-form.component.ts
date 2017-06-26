1b09e5da7ad5f461a9149ac6ba1df08a6cf91718
Dan Ford <daniel.ford@libertymutual.com>
2017-06-26 09:25:54 -0400

    student_class updated

--- src/app/student-class-form/student-class-form.component.ts
+++ src/app/student-class-form/student-class-form.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import 'rxjs/add/operator/switchMap';
+import { Component, OnInit }      from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location }               from '@angular/common';
+
+import { DataService } from '../data.service'
 
 @Component({
   selector: 'app-student-class-form',
@@ -7,9 +12,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentClassFormComponent implements OnInit {
 
-  constructor() { }
+  successMessage: string;
+  errorMessage: string;
+
+  studentclass: object = {};
+
+  getRecordForEdit(){
+    this.route.params
+      .switchMap((params: Params) => this.dataService.getRecord("student_class", +params['id']))
+      .subscribe(studentclass => this.studentclass = studentclass);
+  }
+
+  constructor(
+    private dataService: DataService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
 
   ngOnInit() {
+    this.route.params
+      .subscribe((params: Params) => {
+        (+params['id']) ? this.getRecordForEdit() : null;
+      });
+  
+  }
+
+  saveStudentClass(id){
+    if(typeof id === "number"){
+      this.dataService.editRecord("student_class", this.studentclass, id)
+          .subscribe(
+            studentclass => this.successMessage = "Record updated succesfully",
+            error =>  this.errorMessage = <any>error);
+    }else{
+      this.dataService.addRecord("student_class", this.studentclass)
+          .subscribe(
+            studentrclass => this.successMessage = "Record added succesfully",
+            error =>  this.errorMessage = <any>error);
+    }
+
+    this.studentclass = {};
+    
   }
 
 }
